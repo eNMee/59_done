128fd4041eb0d73110fa1b3549715cfc44d5861f
Nazar <test@gmail.com>
2024-05-22 14:48:21 +0300

    Добавив модуль оновлення проекту та вихід з програми

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from CreateProject import create_project
 import os.path
 from os import path
+from UpdateProject import update_project
+
 def main():
     while True:
         print("1.Створити проект.\n"
-              "2.Змінити проект.\n"
+              "2.Оновити проект.\n"
               "3.Видалити проект.\n"
               "4.Створити завдання для проекту.\n"
               "5.Змінити завдання для проекту.\n"
               "6.Видалити завдання для проекту.\n"
               "7.Призначити відповідальних проекту.\n"
-              "8.Встановити дедлайн проекту.")
+              "8.Встановити дедлайн проекту.\n"
+              "9.Вихід")
 
         choice = input("Введіть ваш вибір: ")
         if choice == "1":
             create_project()
         elif choice == "2":
-            pass
+            update_project()
         elif choice == "3":
             pass
         elif choice == "4":
@@ -29,6 +32,8 @@ def main():
             pass
         elif choice == "8":
             pass
+        elif choice == "9":
+            break
 
 
 main()
