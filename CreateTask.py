15fc0fa6e1fc7eb4dcaa283fcf63d70f1cc1c6b5
Nazar <test@gmail.com>
2024-05-22 15:51:14 +0300

    Добавив можливість створювати дедлайн,статус завдання,та назначати відповідальних осіб в модулі Створення завдання

--- CreateTask.py
+++ CreateTask.py
@@ -5,9 +5,15 @@ from os import path
 def create_task():
     name_project = input("Введіть назву проекта: ")
     if path.exists(f"{name_project}/{name_project}.txt"):
-        with open(f"{name_project}/ {name_project}_Task.txt", "w") as task:
-            text_task = input("Введіть завдання для проекту: ")
-            task.write(text_task)
-            print(f"Task for project {name_project} was created")
+        if not path.exists(f"{name_project}/ {name_project}_Task.txt"):
+            with open(f"{name_project}/ {name_project}_Task_Info.txt", "w") as task:
+                text_task = "Task: " + input("Введіть завдання для проекту: ")
+                deadline = "Deadline: " + input("Введіть deadline: ")
+                status = "Status: " + input("Статус завдання: ")
+                res_person = "Res_person: " + input("Введіть відповідальну особу: ")
+                task.writelines([res_person + "\n", deadline + "\n", status + "\n", text_task])
+                print(f"Task for project {name_project} was created")
+        else:
+            print(f"Task for project {name_project} already exists")
     else:
         print("Created project before created task for project!")
